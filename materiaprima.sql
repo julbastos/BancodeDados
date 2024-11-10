09042f7cfd240dc9025f9f8ad08bb55cd6cd998c
Ana Lima <anaecarlos70@gmail.com>
2024-11-10 15:08:32 -0300

    Create materiaprima.sql
    Insert da matéria prima

---
+++ materiaprima.sql
@@ -0,0 +1,5 @@
+INSERT INTO Materia_Prima (biomateriais, micro_organismos, substancias_minerais, extrato_plantas)
+VALUES
+    ('Colágeno', 'Lactobacillus acidophilus', 'Óxido de magnésio', 'Ginseng'),
+    ('Fibra de carbono', 'Escherichia coli (E. coli)', 'Sulfato de Ferro', 'Camomila'),
+    ('Quitosana', 'Streptomyces', 'Carbonato de Cálcio', 'Hortelã-pimenta');
